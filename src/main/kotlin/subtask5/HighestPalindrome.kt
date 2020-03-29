a9a9ea77e1548cd8f4b9d3e31438ab663641bd3d
Derugal87 <60985658+Derugal87@users.noreply.github.com>
2020-03-29 21:54:50 +0300

    Update HighestPalindrome.kt

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -2,8 +2,21 @@ package subtask5
 
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        val charArray = digitString.toCharArray()
+        var value = 0
+        for (i in charArray.indices) {
+            if (i > n / 2) break
+            if (charArray[i] != charArray[n - i - 1]) {
+                if (charArray[i] == '0') {
+                    charArray[i] = '9'
+                }
+                charArray[n - i - 1] = charArray[i]
+                value++
+            } else if (value > k) {
+                return "-1"
+            }
+        }
+        return String(charArray)
     }
 }
