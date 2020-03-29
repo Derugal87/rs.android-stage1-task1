c38c2577bf8c4d94e08ea8da1cfd1b439f7c0eef
Derugal87 <60985658+Derugal87@users.noreply.github.com>
2020-03-29 19:21:29 +0300

    Update MiniMaxSum.kt

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -1,9 +1,7 @@
 package subtask2
 
 class MiniMaxSum {
-
-    // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        return arrayOf(input.sum() - (input.max() ?: 0), input.sum() - (input.min() ?: 0)).toIntArray()
     }
 }
