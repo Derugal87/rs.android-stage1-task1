c67c01055239f50667869d8136e515a426b6c394
Derugal87 <60985658+Derugal87@users.noreply.github.com>
2020-03-29 20:29:41 +0300

    Update StringParser.kt

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,27 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+
+        val resultString = ArrayList<String>()
+        val brackets = listOf('<', '(', '[', '>', ')', ']')
+        val stringBuilder = StringBuilder(inputString)
+
+        for (n in stringBuilder.length - 1 downTo 0) {
+            if (stringBuilder[n] in brackets.subList(0, 3)) {
+                var lastIndex = 0
+                for (m in n until stringBuilder.length) {
+                    if (stringBuilder[m] == brackets[brackets.indexOf(inputString[n]) + 3]) {
+                        lastIndex = m
+                        stringBuilder[n] = '|'
+                        stringBuilder[lastIndex] = '|'
+                        break
+                    }
+                }
+                resultString.add(0, inputString.substring(n + 1, lastIndex))
+            }
+        }
+        return resultString.toTypedArray()
     }
+
 }
